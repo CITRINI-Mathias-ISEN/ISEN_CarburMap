7b0150a416ae99f9cae50141fd57908c9f216a89
Aymeric le bg du groupe <aymeric.daniel@isen.yncrea.fr>
2022-12-20 16:57:49 +0100

    Création data class + model part 2

--- src/main/kotlin/org/isen/carburmap/ctrl/CarburMapController.kt
+++ src/main/kotlin/org/isen/carburmap/ctrl/CarburMapController.kt
@@ -1,7 +1,20 @@
 package org.isen.carburmap.ctrl
 
 import org.isen.carburmap.model.ICarburMapModel
+import org.isen.carburmap.view.ICarburMapView
 
 class CarburMapController(val model:ICarburMapModel) {
-    //TODO implement
+    val views = mutableListOf<ICarburMapView>()
+
+    fun displayViews(){
+        views.forEach(){
+            it.display()
+        }
+    }
+
+    fun closeView(){
+        views.forEach(){
+            it.close()
+        }
+    }
 }
--- src/main/kotlin/org/isen/carburmap/view/ICarburMapView.kt
+++ src/main/kotlin/org/isen/carburmap/view/ICarburMapView.kt
@@ -1,4 +1,6 @@
 package org.isen.carburmap.view
 
 interface ICarburMapView {
+    fun display()
+    fun close()
 }
