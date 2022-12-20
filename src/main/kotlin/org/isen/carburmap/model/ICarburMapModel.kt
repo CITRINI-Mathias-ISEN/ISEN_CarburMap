2e4201c3dedd966f8535961cd1f964d9ae639948
Aymeric le bg du groupe <aymeric.daniel@isen.yncrea.fr>
2022-12-20 16:57:57 +0100

    Création data class + model part 3

--- src/main/kotlin/org/isen/carburmap/model/ICarburMapModel.kt
+++ src/main/kotlin/org/isen/carburmap/model/ICarburMapModel.kt
@@ -1,5 +1,10 @@
 package org.isen.carburmap.model
 
-interface ICarburMapModel {
+import java.beans.PropertyChangeListener
 
+interface ICarburMapModel {
+    fun register(datatype:String?,listener:PropertyChangeListener)
+    fun unregister(listener:PropertyChangeListener)
+    fun findStation(radius:Long)
+    fun changeCurrentSelection(id:Long)
 }
