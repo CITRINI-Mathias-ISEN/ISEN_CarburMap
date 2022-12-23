d39c1e2f306bb33ecfeb37afe6fd8a9ef4266da2
Mathias Citrini <devilmortar0159@gmail.com>
2022-12-23 11:47:16 +0100

    Changed model to manage the findStationByJSON function

--- src/main/kotlin/org/isen/carburmap/model/impl/DefaultCarburmapModel.kt
+++ src/main/kotlin/org/isen/carburmap/model/impl/DefaultCarburmapModel.kt
@@ -2,7 +2,7 @@ package org.isen.carburmap.model.impl
 
 import com.github.kittinunf.fuel.httpGet
 import org.apache.logging.log4j.kotlin.Logging
-import org.isen.carburmap.data.Station
+import org.isen.carburmap.data.StationsList
 import org.isen.carburmap.model.ICarburMapModel
 import java.beans.PropertyChangeListener
 import java.beans.PropertyChangeSupport
@@ -14,24 +14,31 @@ class DefaultCarburmapModel : ICarburMapModel {
 
     private val pcs = PropertyChangeSupport(this)
 
-    private var station: Station? by Delegates.observable(null) {
+    private var stationsList: StationsList? by Delegates.observable(null) {
             _, oldValue, newValue ->
         logger.info("stationInformation updated")
         //pcs.firePropertyChange(IMyVelibModel.DATATYPE_VELIB, oldValue, newValue)
     }
 
-    override fun findStation(x:Double, y:Double, radius:Long) {
-            "https://public.opendatasoft.com/api/records/1.0/search/?dataset=prix-des-carburants-j-1&q=&rows=1&geofilter.distance=$x%2C+$y%2C+$radius"
+    override fun findStationByJSON(x:Double, y:Double, radius:Long) {
+            "https://data.economie.gouv.fr//api/records/1.0/search/?dataset=prix-carburants-fichier-instantane-test-ods-copie&q=&rows=-1&geofilter.distance=$x%2C+$y%2C+$radius"
             .httpGet()
-            .responseObject(Station.Deserializer()) { request, response, result ->
+            .responseObject(StationsList.Deserializer()) { request, response, result ->
                 val (data, error) = result
                 if (data != null) {
-                    station = data
-                    println(station!!.records[0].fields)
+                    stationsList = data
+                    println(stationsList!!.records[0].fields)
                 } else {
+                    println(error)
                     logger.warn("Be careful data is void $error")
                 }
             }
+    }
+
+    override fun findStationByXML(x:Double, y:Double, radius:Long) {
+        // Get the file from resources folder
+        val file = ClassLoader.getSystemClassLoader().getResource("./PrixCarburants_instantane.xml")
+        val xml = file.readText()
     }
 
     override fun register(datatype:String?, listener:PropertyChangeListener){
