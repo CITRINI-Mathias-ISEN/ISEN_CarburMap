1b1f28d42773a0de7844924ff6d8c35dfab7ce05
Mathias Citrini <devilmortar0159@gmail.com>
2022-12-23 11:46:11 +0100

    Changed name and structure of Station object

---
+++ src/main/kotlin/org/isen/carburmap/data/StationsList.kt
@@ -0,0 +1,36 @@
+package org.isen.carburmap.data
+
+import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.google.gson.Gson
+
+data class StationsList(val records:List<Record>) {
+    class Deserializer : ResponseDeserializable<StationsList> {
+        override fun deserialize(content: String): StationsList = Gson().fromJson(content, StationsList::class.java)
+    }
+}
+
+data class Record(val fields:Fields)
+
+data class Fields(
+    val prix_valeur:Double,
+    val prix_maj: String,
+    val cp: String,
+    val services_service: String,
+    val horaires: String,
+    val com_code: Int,
+    val id: Long,
+    val com_arm_name: String,
+    val adresse: String,
+    val horaires_automate_24_24: String,
+    val pop: String,
+    val epci_code: Long,
+    val reg_name: String,
+    val brand: String,
+    val dep_name: String,
+    val epci_name: String,
+    val prix_nom:String,
+    val geom: Array<Double>,
+    val dist: Double
+)
+
+
