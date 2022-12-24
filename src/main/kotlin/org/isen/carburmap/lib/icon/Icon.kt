6a0e8e4015520869ff974efeb91b9c4a150ba259
Kyllian Schwedt <kyllian@schwedt.fr>
2022-12-24 01:20:06 +0100

    Icon data class + fix typo

--- src/main/kotlin/org/isen/carburmap/lib/icon/Icon.kt
+++ src/main/kotlin/org/isen/carburmap/lib/icon/Icon.kt
@@ -3,4 +3,4 @@ package org.isen.carburmap.lib.icon
 import java.awt.Image
 import java.awt.image.BufferedImage
 
-class Icon(val name:String, val path:String, val bufferedImage: BufferedImage, val image: Image, val smallImage: Image, val bigImage: Image)
+data class Icon(val name:String, val path:String, val bufferedImage: BufferedImage, val image: Image, val smallImage: Image, val bigImage: Image)
--- src/main/kotlin/org/isen/carburmap/lib/icon/IconManager.kt
+++ src/main/kotlin/org/isen/carburmap/lib/icon/IconManager.kt
@@ -6,12 +6,12 @@ import javax.imageio.ImageIO
 class IconManager private constructor() {
     private val iconMaps: MutableMap<String, Icon> = mutableMapOf()
     companion object {
-        private var INSTANCE: IconManager? = null;
+        private var INSTANCE: IconManager? = null
         fun getInstance(): IconManager {
             if (INSTANCE == null) {
                 INSTANCE = IconManager()
             }
-            return INSTANCE!!;
+            return INSTANCE!!
         }
     }
 
@@ -29,7 +29,7 @@ class IconManager private constructor() {
             val img = bufferedImage.getScaledInstance(48,48, Image.SCALE_SMOOTH)
             val smallImg = bufferedImage.getScaledInstance(32,32, Image.SCALE_SMOOTH)
             val bigImg = bufferedImage.getScaledInstance(72,72, Image.SCALE_SMOOTH)
-            iconMaps[path] = Icon(path, path.split('/').last(),bufferedImage, img, smallImg, bigImg)
+            iconMaps[path] = Icon(path, path.split('/').last(), bufferedImage, img, smallImg, bigImg)
             return iconMaps[path]!!
         } catch (e: Exception) {
             e.printStackTrace()
