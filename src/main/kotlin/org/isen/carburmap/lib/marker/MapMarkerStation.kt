32a0511238470a7b263c8400d93bec5b3c8ac454
Kyllian Schwedt <kyllian@schwedt.fr>
2022-12-27 02:13:14 +0100

    + MarkerStation

---
+++ src/main/kotlin/org/isen/carburmap/lib/marker/MapMarkerStation.kt
@@ -0,0 +1,7 @@
+package org.isen.carburmap.lib.marker
+
+import org.isen.carburmap.data.Station
+import org.openstreetmap.gui.jmapviewer.Coordinate
+
+class MapMarkerStation(val station: Station, imgPath: String) : MapMarkerSelect(Coordinate(station.coordonnees[0], station.coordonnees[1]), imgPath) {
+}
