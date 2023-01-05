e0b9bccdb9d41ce92600c67f8b7325cfbd6f0ebc
Mathias Citrini <devilmortar0159@gmail.com>
2023-01-05 22:41:34 +0100

    Create a REAL test because Kyllian doesn't know how to do !

--- src/test/kotlin/org/isen/carburmap/lib/geo/GeoCodeTest.kt
+++ src/test/kotlin/org/isen/carburmap/lib/geo/GeoCodeTest.kt
@@ -1,14 +1,16 @@
 package org.isen.carburmap.lib.geo
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class GeoCodeTest{
     @Test
     fun testGetLatLonFromAddress(){
         val geoCode = GeoCode()
-        val latLon = geoCode.getFromAddress("Isen Toulon")
-        latLon.forEach {
-            println("Lat: ${it.lat} Lon: ${it.lon} Display name: ${it.display_name}")
-        }
+        val latLon = geoCode.getFromAddress("Parc des Princes, 24, Rue du Commandant Guilbaud, Quartier d'Auteuil")
+        assertEquals(48.84, latLon[0].lat, 0.005)
+        assertEquals(2.25, latLon[0].lon, 0.005)
+        assertEquals("Parc des Princes, 24, Rue du Commandant Guilbaud, Quartier d'Auteuil, Paris 16e Arrondissement, Paris, Île-de-France, France métropolitaine, 75016, France", latLon[0].display_name)
+        assertEquals(119871247 , latLon[0].place_id)
     }
 }
