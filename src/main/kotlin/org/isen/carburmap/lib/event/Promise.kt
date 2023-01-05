b60a5c25aee9069648b2a90d9f3bcf9dee09a1c6
Kyllian Schwedt <kyllian@schwedt.fr>
2023-01-05 20:36:57 +0100

    Promess for station list fragments on multiple threads

---
+++ src/main/kotlin/org/isen/carburmap/lib/event/Promise.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023, Kyllian SCHWEDT and/or its affiliates. All rights reserved.
+ * PROPRIETARY/CONFIDENTIAL
+ *
+*/
+package org.isen.carburmap.lib.event
+
+import org.apache.logging.log4j.kotlin.Logging
+
+class Promise(private val promisePool: PromisePool) {
+    companion object : Logging
+    var result: Any? = null
+        set(value) {
+            field = value
+            promisePool.fulfill(this)
+        }
+    enum class State {
+        PENDING,
+        FULFILLED,
+        REJECTED
+    }
+
+    var state: State = State.PENDING
+        set(value) {
+            field = value
+            if (value == State.FULFILLED) {
+                fulfillCallback()
+            } else if (value == State.REJECTED) {
+                logger.warn("Promise rejected")
+                fulfillCallback()
+            }
+        }
+
+    private fun fulfillCallback(){
+        promisePool.fulfill(this)
+    }
+}
---
+++ src/main/kotlin/org/isen/carburmap/lib/event/PromisePool.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023, Kyllian SCHWEDT and/or its affiliates. All rights reserved.
+ * PROPRIETARY/CONFIDENTIAL
+ *
+*/
+package org.isen.carburmap.lib.event
+
+import org.apache.logging.log4j.kotlin.Logging
+import org.isen.carburmap.data.StationsList
+
+class PromisePool(private val finalCall: (List<StationsList>) -> Unit) {
+    companion object : Logging
+    private val promises = ArrayDeque<Promise>()
+
+    private val fulfilledPromises = mutableListOf<Promise>()
+    fun fulfill(promise: Promise) {
+        fulfilledPromises.add(promise)
+        promises.remove(promise)
+        if (promises.isEmpty()) {
+            finalCall(fulfilledPromises.mapNotNull { if(it.result is StationsList) it.result as StationsList else null })
+            logger.info("All promises fulfilled total: ${fulfilledPromises.size}")
+        } else {
+            logger.debug("Promise fulfilled, ${promises.size} remaining")
+        }
+    }
+
+    fun add(promise: Promise) {
+        promises.add(promise)
+    }
+
+    fun createPromise(): Promise {
+        val promise = Promise(this)
+        add(promise)
+        return promise
+    }
+}
